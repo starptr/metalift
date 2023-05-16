d9b6a7c94f9b27e7395b82c522b44485170e12b4
Yuto Nishida <yuto@berkeley.edu>
2023-05-16 14:55:59 -0700

    Add PS

--- tests/conv2d.py
+++ tests/conv2d.py
@@ -52,7 +52,7 @@ def ml_min(a, b):
 def ml_dotprod2x2(mat, kernel, i, j):
     return Call(DOTPROD2X2, Int(), mat, kernel, i, j)
 
-def ml_conv2d2x2(mat, kernel, i, j):
+def ml_conv2d2x2(mat, kernel):
     return Call(CONV2D2X2, ListT(ListT(Int())), mat, kernel)
 
 def ml_conv2d2x2_helper(mat, kernel, i):
@@ -63,20 +63,31 @@ def ml_conv2d2x2_inner(mat, kernel, i, j):
 
 def grammar(ci: CodeInfo):
     name = ci.name
+    print("NAME: ", name)
     print("INV VARS MV HERE")
     print(*ci.modifiedVars)
+    print("MV len: ", len(ci.modifiedVars))
     print("INV VARS RV HERE")
     print(*ci.readVars)
 
-    unknown_const = Choose(IntLit(-2), IntLit(-1), IntLit(0), IntLit(1), IntLit(2))
+    unknown_const = Choose(IntLit(0), IntLit(1), IntLit(2))
     kernel_r1 = ml_list_prepend(unknown_const, ml_list_prepend(unknown_const, ml_list_empty()))
     kernel_r2 = kernel_r1
     kernel = ml_list_list_prepend(kernel_r1, ml_list_list_prepend(kernel_r2, ml_list_list_empty()))
 
-    if name.startswith("inv"):
+    if name.startswith("inv0"):
+        return Synth(name, Eq(ci.readVars[0], ci.modifiedVars[2]), *ci.modifiedVars, *ci.readVars)
+    elif name.startswith("inv1"):
         return Synth(name, Eq(ci.readVars[0], ci.modifiedVars[2]), *ci.modifiedVars, *ci.readVars)
     else:
-        return Synth(name, Eq(ci.readVars[0], ci.modifiedVars[0]), *ci.modifiedVars, *ci.readVars)
+        an_output = ci.modifiedVars[0]
+        an_input = ci.readVars[0]
+        valid = And(Gt(ml_list_list_length(an_input), IntLit(1)),
+                    Gt(ml_list_length(ml_list_list_get(an_input, IntLit(0))), IntLit(1)))
+        ans = ml_conv2d2x2(an_input, kernel)
+        check_ans = Eq(ans, an_output)
+        summary = Implies(valid, check_ans)
+        return Synth(name, summary, *ci.modifiedVars, *ci.readVars)
 
 def targetLang():
     mat = Var("mat", ListT(ListT(Int())))
