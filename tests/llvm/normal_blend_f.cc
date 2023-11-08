88056622f064aa73a2419d0af7f17d6fe0855095
Yuto Nishida <yuto@berkeley.edu>
2023-11-07 17:36:22 -0800

    Rename nbf

--- tests/llvm/normal_blend_f.cc
+++ tests/llvm/normal_blend_f.cc
@@ -1,6 +1,6 @@
 #include "list.h"
 
-std::vector<int> test(std::vector<int> base, std::vector<int> active, int opacity) 
+std::vector<int> normal_blend_f(std::vector<int> base, std::vector<int> active, int opacity) 
 {
   std::vector<int> out;
   for (int i = 0; i < base.size(); ++i)
